94a13872874d12d700a3af5e72d973ba7467d69a
Kellis Kincaid (khk) <kincaidkc@ornl.gov>
2024-01-24 13:41:41 -0500

    Basic refinement field class written but not compiled or tested. Pausing work to implement helper functions in the beam segment class.

---
+++ applications/solvers/additiveFoam/movingHeatSource/refinementControllers/refinementController/refinementController.C
@@ -0,0 +1,188 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+                Copyright (C) 2023 Oak Ridge National Laboratory                
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "refinementController.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(refinementController, 0);
+    defineRunTimeSelectionTable(refinementController, dictionary);
+}
+
+const Foam::word Foam::refinementController::refinementControllerDictName
+(
+    "refinementControllerDict"
+);
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+Foam::IOobject Foam::refinementController::createIOobject
+(
+    const dictionary& dict,
+    const fvMesh& mesh
+) const
+{
+    typeIOobject<IOdictionary> io
+    (
+        dict.name(),
+        mesh.time().constant(),
+        mesh.thisDb(),
+        IOobject::MUST_READ,
+        IOobject::NO_WRITE
+    );
+
+    if (io.headerOk())
+    {
+        io.readOpt() = IOobject::MUST_READ_IF_MODIFIED;
+        return io;
+    }
+    else
+    {
+        io.readOpt() = IOobject::NO_READ;
+        return io;
+    }
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::refinementController::refinementController
+(
+    const word& type,
+    const PtrList<heatSourceModel> sources,
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    IOdictionary(createIOobject(dict, mesh)),
+
+    sources_(sources),
+    heatSourceDict_(dict),
+    mesh_(mesh),
+    refinementDict_(sourceDict_.optionalSubDict(type + "Coeffs")),
+    refine_ = refinementDict_.lookup<bool>("refine"),
+    refinementField_
+    (
+        IOobject
+        (
+            "refinementField",
+            mesh_.time().timeName(),
+            mesh_,
+            refine_ ? IOobject::READ_IF_PRESENT : IOobject::NO_READ,
+            refine_ ? IOobject::AUTO_WRITE : IOobject::NO_WRITE
+        ),
+        mesh_,
+        dimensionedScalar(dimless, 0.0)
+    ),
+    resolveTail_(false),
+    persistence_(0.0)
+{
+    if (refine_)
+    {
+        resolveTail_
+            = refinementDict_.lookupOrDefault<bool>("resolveTail", false);
+        persistence_
+            = refinementDict_.lookupOrDefault<scalar>("persistence", 0.0);
+    }
+    
+    solidificationTime_
+        = new volScalarField
+              (
+                  IOobject
+                  (
+                      "solidificationTime",
+                      mesh_.time().timeName(),
+                      mesh_,
+                      persistence_ > 0.0
+                          ? IOobject::READ_IF_PRESENT : IOobject::NO_READ,
+                      persistence_ > 0.0
+                          ? IOobject::AUTO_WRITE : IOobject::NO_WRITE
+                  ),
+                  mesh_,
+                  dimensionedScalar(dimTime, LARGE)
+              );
+}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+void Foam::refinementController::initializeRefinementField()
+{
+    //- Initialize refinement field to capture melt pool
+    if (resolveTail_)
+    {
+        const volScalarField alphaSol
+            = mesh_.lookupObject<volScalarField>("alpha1");
+            
+        const volScalarField alphaSol0 = alphaSol.oldTime();
+        
+        refinementField_ = pos0(1.0 - alphaSol);
+        
+        if (persistence_ > 0.0)
+        {
+            const scalar currTime = mesh_.time().value();
+        
+            //- Update solidification time field
+            forAll(mesh_.C(), celli)
+            {
+                if ((alphaSol[celli] > 0.99) && (alphaSol0[celli] < 0.99))
+                {
+                    //- Update solidification time field
+                    solidificationTime_[celli] = currTime;
+                }
+                
+                //- Ensure recently solidified regions stay refined
+                //  for persistence time
+                if (solidificationTime_[celli] + persistence_ < currTime)
+                {
+                    refinementField_[celli] = 1.0;
+                }
+            }
+        }        
+    }
+    
+    //- Reset refinement field if no tail capturing is desired
+    else
+    {
+        refinementField_ = dimensionedScalar(dimless, 0.0);
+    }
+}
+
+bool Foam::refinementController::read()
+{
+    if (regIOobject::read())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// ************************************************************************* //
---
+++ applications/solvers/additiveFoam/movingHeatSource/refinementControllers/uniformIntervals/uniformIntervals.C
@@ -0,0 +1,170 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+                Copyright (C) 2023 Oak Ridge National Laboratory                
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "uniformIntervals.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace refinementControllers
+{
+    defineTypeNameAndDebug(uniformIntervals, 0);
+    addToRunTimeSelectionTable(refinementController, uniformIntervals, dictionary);
+}
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::refinementControllers::uniformIntervals::uniformIntervals
+(
+    const PtrList<heatSourceModel> sources,
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    heatSourceModel(typeName, sources, dict, mesh)
+{
+    intervals_ = refinementDict_.lookup<int>("intervals");
+    dt_ = mesh_.time().endTime() / intervals_;
+    updateTime_ = 0.0;
+}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::refinementControllers::uniformIntervals::update()
+{
+    if (mesh_.time() >= updateTime_)
+    {
+        //- Initialize refinement marker field in base class
+        refinementController::initializeRefinementField();
+    
+        //- Update next refinement time
+        const scalar currTime = mesh_.time();
+        updateTime_ = currTime + dt_;
+        
+        //- Buffer for bounding box calculations
+        const vector extend = 1e-10 * vector::one;
+        
+        //- Update refinement marker field
+        forAll(sources_, i)
+        {
+            //- Create dummy time for forward march scheme
+            scalar beamTime = currTime;
+            
+            //- March along beam path until next update time is reached
+            while (beamTime <= updateTime_)
+            {
+                //- Initialize time step size and velocity
+                scalar beamDt = 0.0;
+                
+                //- Calculate max beam timestep based on velocity for
+                //  moving beams, or time until next path for stationary
+                scalar beamMode = sources_[i].beam().mode(beamTime);
+                
+                if (beamMode == 1.0)
+                {
+                    beamDt = sources_[i].beam().D2sigma()
+                             / sources_[i].beam().parameter(beamTime);
+                }
+                
+                else
+                {
+                    beamDt = sources_[i].beam().timeToNextPath(beamTime);
+                }
+                
+                //- Ensure final time step refines at next update time
+                beamTime = min(beamTime + dt, updateTime_);
+                
+                //- Mark cells near beam at current time/location for
+                //  refinement
+                vector beamDims = sources_[i].dimensions();
+                vector currPos = sources_[i].position(beamTime);
+                treeBoundBox beamBb(currPos - beamDims, currPos + beamDims);
+                
+                forAll(mesh_.C(), celli)
+                {
+                    //- Don't redo checks if cell is already marked
+                    if (refinementField_[celli] > 0.0)
+                    {
+                        continue;
+                    }
+                    
+                    treeBoundBox cellBb(point::max, point::min);
+                    
+                    const labelList& vertices = mesh_.cellPoints()[celli];
+                    
+                    forAll(vertices, j)
+                    {
+                        cellBb.min()
+                            = min(cellBb.min(), points[vertices[j]] - extend);
+                        cellBb.max()
+                            = max(cellBb.max(), points[vertices[k]] + extend);
+                    }
+                    
+                    if (cellBb.overlaps(beamBb))
+                    {
+                        refinementField_[celli] = 1.0;
+                    }
+                }
+            }            
+        }
+        
+        refinementField_ = pos(refinementField_);
+        
+        //- Return true to call mesh.update()
+        return true;
+    }
+    
+    else
+    {
+        return false;
+    }
+}
+
+
+bool Foam::refinementControllers::uniformIntervals::read()
+{
+    if (refinementController::read())
+    {
+        refinementDict_ = optionalSubDict(type() + "Coeffs");
+
+        //- Mandatory entries
+        refinementDict_.lookup("intervals") >> intervals_;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// ************************************************************************* //
