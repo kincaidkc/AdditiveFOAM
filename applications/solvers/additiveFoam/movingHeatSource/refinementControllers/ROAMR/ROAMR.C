530b0d866374a031248b89ce9671a3d35076f000
Kellis Kincaid (khk) <kincaidkc@ornl.gov>
2024-08-28 11:27:13 -0400

    First draft of new initial guess

--- applications/solvers/additiveFoam/movingHeatSource/refinementControllers/ROAMR/ROAMR.C
+++ applications/solvers/additiveFoam/movingHeatSource/refinementControllers/ROAMR/ROAMR.C
@@ -47,61 +47,119 @@ Foam::refinementControllers::ROAMR::ROAMR
     uniformIntervals(sources, dict, mesh, true),
 
     coeffs_(refinementDict_.optionalSubDict(typeName + "Coeffs")),
-    cellsPerProc_(coeffs_.lookupOrDefault<int>("cellsPerProc", 10000))
+    cellsPerProc_(coeffs_.lookupOrDefault<int>("cellsPerProc", 10000)),
+    lastRefi_(0)
 {
-    //- Get average cell volume and cross-sectional area
-    label totalCells = mesh_.nCells();
-    reduce(totalCells, sumOp<label>());
-    scalar vAvg = gSum(mesh_.V()) / totalCells;
+    //- Calculate total mesh volume
+    scalar Vtot = 0.0;
+    forAll(mesh_.C(), celli)
+    {
+        Vtot += mesh_.V()[celli];
+    }
+    reduce(Vtot, sumOp<scalar>());
+    Info << "Total mesh volume: " << Vtot << endl;
     
-    //- Find longest path and estimate total scan area
-    scalar maxLen = 0.0;
-    scalar maxDim = 0.0;
-    scalar scanArea = 0.0;
+    //- Find average cell dimension
+    scalar dx = Foam::pow(Vtot / mesh_.nCells(), 1.0 / 3.0);
+    
+    //- Calculate the bounding box for each cell
+    List<treeBoundBox> cellBbs(mesh_.nCells());    
+    const pointField& points = mesh_.points();
+    const vector extend = 1e-10 * vector::one;
+    
+    forAll(mesh_.cells(), celli)
+    {
+        treeBoundBox cellBb(point::max, point::min);
 
+        const labelList& vertices = mesh_.cellPoints()[celli];
+
+        forAll(vertices, j)
+        {
+            cellBb.min()
+                = min(cellBb.min(), points[vertices[j]] - extend);
+            cellBb.max()
+                = max(cellBb.max(), points[vertices[j]] + extend);
+        }
+
+        cellBbs[celli] = cellBb;
+    }
+    
+    //- Calculate scan path volume
     forAll(sources_, i)
     {
-        maxLen = max(sources_[i].beam().totalLength(), maxLen);
+        const movingBeam& beam_ = sources_[i].beam();
         
-        treeBoundBox beamBb
-        (
-            min(vector::zero, boundingBox_.min()),
-            max(1.5 * sources_[i].dimensions(), boundingBox_.max())
-        );
-        
-        point bbMin = beamBb.min();
-        point bbMax = beamBb.max();
-        
-        scalar bbMaxDim = max(bbMax[0] - bbMin[0], bbMax[1] - bbMin[1]);
+        scalar time_ = 0.0;
 
-        maxDim = max(maxDim, bbMaxDim);
-        
-        scanArea +=
-            4.0 * bbMaxDim
-          * Foam::pow(Foam::pow(bbMax[2] - bbMin[2], 2.0), 0.5);
+        vector offset_ = 1.5*sources_[i].dimensions();
+
+        while (time_ < beam_.endTime())
+        {
+            vector position_ = beam_.position(time_);
+
+            treeBoundBox beamBb
+            (
+                position_ - min(offset_, boundingBox_.min()),
+                position_ + max(offset_, boundingBox_.max())
+            );
+            
+            forAll(mesh_.cells(), celli)
+            {
+                if (refinementField_[celli] > 0)
+                {
+                    // Do nothing, cell already marked for refiment
+                }
+                else if (cellBbs[celli].overlaps(beamBb))
+                {
+                    refinementField_[celli] = 1;
+                }
+            }
+            
+            //- Calculate time step required to resolve beam motion on mesh
+            label index_ = beam_.findIndex(time_);
+            segment path_ = beam_.getSegment(index_);
+            scalar timeToNextPath_ = path_.time() - time_;
+
+            //- If the path end time is directly hit, step to next path
+            while (mag(timeToNextPath_) < small)
+            {
+                index_ = index_ + 1;
+                path_ = beam_.getSegment(index_);
+                timeToNextPath_ = path_.time() - time_;
+            }
+
+            scalar dt_ = timeToNextPath_;
+
+            if (path_.mode() == 0)
+            {
+                const scalar scanTime_ =
+                    sources_[i].D2sigma() / path_.parameter();
+
+                dt_ = min(timeToNextPath_, scanTime_);
+            }
+
+            time_ += dt_;
+        }
     }
-
-    //- Calculate maximum number of intervals or shortest interval
-    //  size to maintain at least 1 bounding box between updates
-    scalar maxIntervals = maxLen / maxDim;
-    minIntervalTime_ = endTime_ / maxIntervals;
-
-    //- Calculate number of intervals to optimize cells per processor
+    
+    scalar Vref = fvc::domainIntegrate(refinementField_).value();
+    Info << "Total volume of refined scan path: " << Vref << endl;
+    
+    //- Calculate number of cells required to fully resolve entire scan path
+    scalar nTot1 = (Vtot + Vref * (Foam::pow(2.0, 3.0 * nLevels_) - 1.0))
+                   / Foam::pow(dx, 3.0);
+            
+    //- Calculate target mesh size
     scalar targetCells = Pstream::nProcs() * cellsPerProc_;
-
-    if (targetCells > totalCells)
-    {
-        intervals_ =
-            maxLen * scanArea / vAvg
-          / (targetCells - totalCells)
-          * (Foam::pow(2.0, 3.0 * nLevels_) - 1.0);
-    }
-
-    //- Bound number of intervals between 1 and maxIntervals
-    intervals_ = max(min(intervals_, maxIntervals), 1.0);
-
-    Info << "Setting initial number of intervals to: " << intervals_ << endl;
-
+    
+    //- Estimate number of intervals
+    intervals_ = max(nTot1 / targetCells, 1.0);
+    
+    //- TODO: set minimum interval size?
+    
+    Info << "Estimated that " << intervals_
+         << " intervals are required." << endl;
+         
     //- Set number of intervals in uniformIntervals class    
     intervalTime_ = endTime_ / intervals_;
 
@@ -113,16 +171,26 @@ Foam::refinementControllers::ROAMR::ROAMR
 
 bool Foam::refinementControllers::ROAMR::update(const bool& force)
 {
-    //- Update if mesh time equals update time
-    //  OR if time index is equal to the max refinement level.
-    //  This second condition adjusts the mesh after the guess at the first
-    //  refinement interval size to prevent an overly long first interval.
-    if ((updateTime_ - mesh_.time().value() < small)
-        ||
-        (mesh_.time().timeIndex() == nLevels_ + 1))
+    //- Check number of cells in mesh against desired number of cells
+    bool forceUpdate = false;
+    const label nCells = returnReduce(mesh_.nCells(), sumOp<label>());
+    const scalar ratio = nCells / (cellsPerProc_ * Pstream::nProcs());
+
+    if (((ratio > 1.5) || (ratio < 0.5))
+        && 
+        (mesh_.time().timeIndex() >= (lastRefi_ + nLevels_ + 1)))
+    {
+        Info << "Mesh contains " << nCells << " cells, which is " << ratio
+             << " desired number of cells. Forcing mesh update..." << endl;
+        forceUpdate = true;
+    }
+
+    //- Update if mesh time equals update time or if the forceUpdate flag is
+    //  set due to an improperly sized mesh
+    if ((updateTime_ - mesh_.time().value() < small) || (forceUpdate))
     {
         //- Guard against rescaling until full refinement is reached
-        if (mesh_.time().timeIndex() >= nLevels_ + 1)
+        if (mesh_.time().timeIndex() >= (lastRefi_ + nLevels_ + 1))
         {
             //- Scale interval size based on current cells/proc
             label totalCells = mesh_.nCells();
