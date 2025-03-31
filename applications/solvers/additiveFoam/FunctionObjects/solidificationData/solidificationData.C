1ac6136d4ed4ff6116221da7f87745e8c4f4d090
Kellis Kincaid (khk) <kincaidkc@ornl.gov>
2025-03-31 16:58:54 -0400

    working version with TAM

--- applications/solvers/additiveFoam/FunctionObjects/solidificationData/solidificationData.C
+++ applications/solvers/additiveFoam/FunctionObjects/solidificationData/solidificationData.C
@@ -69,8 +69,10 @@ Foam::functionObjects::solidificationData::solidificationData
     fvMeshFunctionObject(name, runTime, dict),
     dict_(dict),
     AMR_(dict_.lookupOrDefault<bool>("AMR", false)),
+    writeCsv_(dict_.lookupOrDefault<bool>("writeCsv", false)),
+    bb_(point::min, point::max),
+    nBgPts_(0, 0, 0),
     refinedSize_(0.0),
-    trackMelting_(dict_.lookupOrDefault<bool>("trackMelting", false)),
     Tl_(dict_.lookup<scalar>("Tl")),
     T_(mesh_.lookupObject<VolField<scalar>>("T")),
     dTdt_
@@ -93,7 +95,7 @@ Foam::functionObjects::solidificationData::solidificationData
             "R",
             mesh_.time().timeName(),
             mesh_,
-            IOobject::READ_IF_PRESENT,
+            AMR_ ? IOobject::NO_READ : IOobject::READ_IF_PRESENT,
             AMR_ ? IOobject::NO_WRITE : IOobject::AUTO_WRITE
         ),
         mesh_,
@@ -106,7 +108,7 @@ Foam::functionObjects::solidificationData::solidificationData
             "G",
             mesh_.time().timeName(),
             mesh_,
-            IOobject::READ_IF_PRESENT,
+            AMR_ ? IOobject::NO_READ : IOobject::READ_IF_PRESENT,
             AMR_ ? IOobject::NO_WRITE : IOobject::AUTO_WRITE
         ),
         mesh_,
@@ -119,9 +121,8 @@ Foam::functionObjects::solidificationData::solidificationData
             "tm",
             mesh_.time().timeName(),
             mesh_,
-            IOobject::READ_IF_PRESENT,
-            AMR_ ? IOobject::NO_WRITE
-                 : (trackMelting_ ? IOobject::AUTO_WRITE : IOobject::NO_WRITE)
+            AMR_ ? IOobject::NO_READ : IOobject::READ_IF_PRESENT,
+            AMR_ ? IOobject::NO_WRITE : IOobject::AUTO_WRITE
         ),
         mesh_,
         dimensionedScalar(dimTime, 0.0)
@@ -133,9 +134,21 @@ Foam::functionObjects::solidificationData::solidificationData
             "ts",
             mesh_.time().timeName(),
             mesh_,
-            IOobject::READ_IF_PRESENT,
-            AMR_ ? IOobject::NO_WRITE
-                 : (trackMelting_ ? IOobject::AUTO_WRITE : IOobject::NO_WRITE)
+            AMR_ ? IOobject::NO_READ : IOobject::READ_IF_PRESENT,
+            AMR_ ? IOobject::NO_WRITE : IOobject::AUTO_WRITE
+        ),
+        mesh_,
+        dimensionedScalar(dimTime, 0.0)
+    ),
+    tam_
+    (
+        IOobject
+        (
+            "tam",
+            mesh_.time().timeName(),
+            mesh_,
+            AMR_ ? IOobject::NO_READ : IOobject::READ_IF_PRESENT,
+            AMR_ ? IOobject::NO_WRITE : IOobject::AUTO_WRITE
         ),
         mesh_,
         dimensionedScalar(dimTime, 0.0)
@@ -146,6 +159,50 @@ Foam::functionObjects::solidificationData::solidificationData
     if (AMR_)
     {
         refinedSize_ = dict_.lookup<scalar>("refinedSize");
+        bb_ = dict_.lookup<treeBoundBox>("boundingBox");
+        
+        //- Resize solidification data to size of background mesh
+        nBgPts_[0] = static_cast<int>((bb_.max()[0] - bb_.min()[0]) / refinedSize_ + 0.5);
+        nBgPts_[1] = static_cast<int>((bb_.max()[1] - bb_.min()[1]) / refinedSize_ + 0.5);
+        nBgPts_[2] = static_cast<int>((bb_.max()[2] - bb_.min()[2]) / refinedSize_ + 0.5);
+        
+        Info << "Background mesh measures " << nBgPts_[0] << " by "
+             << nBgPts_[1] << " by " << nBgPts_[2] << endl;
+        
+        int numPts
+            = static_cast<int>(((bb_.max()[0] - bb_.min()[0])
+                              * (bb_.max()[1] - bb_.min()[1])
+                              * (bb_.max()[2] - bb_.min()[2])
+                              / Foam::pow(refinedSize_, 3.0) + 0.5));
+            
+        solidData_.resize(numPts);
+        
+        // Populate solidification data with cell center locations
+        label index = 0;
+        
+        for (int z = 0; z < nBgPts_[2]; ++z)
+        {
+            for (int y = 0; y < nBgPts_[1]; ++y)
+            {
+                for (int x = 0; x < nBgPts_[0]; ++x)
+                {
+                    List<scalar> datai(8);
+                    
+                    datai[0] = bb_.min()[0] + x * refinedSize_;
+                    datai[1] = bb_.min()[1] + y * refinedSize_;
+                    datai[2] = bb_.min()[2] + z * refinedSize_;
+                    datai[3] = 0.0; // Temperature gradient
+                    datai[4] = 0.0; // Cooling rate
+                    datai[5] = 0.0; // Melting time
+                    datai[6] = 0.0; // Solidification time
+                    datai[7] = 0.0; // Time above melting
+                    
+                    solidData_[index] = datai;
+                    
+                    ++index;
+                }
+            }
+        }
     }
 
     dTdt_ = fvc::ddt(T_);
@@ -177,6 +234,10 @@ bool Foam::functionObjects::solidificationData::execute()
     //- Get current time
     const scalar& time = mesh_.time().value();
     const scalar& deltaT = mesh_.time().deltaTValue();
+    
+    const auto& meshC = mesh_.C();
+    const auto& meshV = mesh_.V();
+    const pointField& points = mesh_.points();
 
     //- Get old temperature and time derivative of temperature
     const volScalarField& T0_ = T_.oldTime();
@@ -193,72 +254,108 @@ bool Foam::functionObjects::solidificationData::execute()
 
     label nSolidificationEvents = 0;
     label nMeltingEvents = 0;
+    label ptNum = 0;
 
-    forAll(mesh_.C(), celli)
+    forAll(meshC, celli)
     {
+        //- Check that current cell is within bounding box
+        treeBoundBox cellBb(point::max, point::min);
+        
+        const labelList& vertices = mesh_.cellPoints()[celli];
+        
+        forAll(vertices, i)
+        {
+            cellBb.min() = min(cellBb.min(), points[vertices[i]]);
+            cellBb.max() = max(cellBb.max(), points[vertices[i]]);
+        }
+        
+        if (!cellBb.overlaps(bb_))
+        {
+            continue;
+        }
+        
         //- Check that cells are in max refinement level for AMR cases
         if (AMR_)
         {
-            if (mesh_.V()[celli] > Vr)
+            if (meshV[celli] > Vr)
             {
                 continue;
+            }
+            else
+            {
+                //- Find index of current location in structured mesh
+                const vector& C = meshC[celli];
+                int nx = static_cast<int>(((C[0] - bb_.min()[0] - refinedSize_ / 2.0)
+                            / refinedSize_ + 0.5));
+                int ny = static_cast<int>(((C[1] - bb_.min()[1] - refinedSize_ / 2.0)
+                            / refinedSize_ + 0.5));
+                int nz = static_cast<int>(((C[2] - bb_.min()[2] - refinedSize_ / 2.0)
+                            / refinedSize_ + 0.5));
+                
+                ptNum = static_cast<int>((nz * nBgPts_[0] * nBgPts_[1]
+                               + ny * nBgPts_[0] + nx + 0.5));
             }
         }
         
         const scalar T = T_[celli];
         const scalar T0 = T0_[celli];
+        
+        if (T >= Tl_)
+        {
+            if (AMR_)
+            {
+                solidData_[ptNum][7] += deltaT;
+            }
+            else
+            {
+                tam_[celli] += deltaT;
+            }
+        }
 
         //- Check for solidification events
         if ((T0 > Tl_) && (T <= Tl_))
         {
             if (AMR_)
-            {
-                vector C = mesh_.C()[celli];
-
-                List<scalar> eventi(6);
-
-                eventi[0] = C[0];
-                eventi[1] = C[1];
-                eventi[2] = C[2];
-                eventi[3] = time;
-                eventi[4] = dTdt_.oldTime()[celli];
-                eventi[5] = gradT[celli];
-
-                solidificationEvents_.append(eventi);
+            {                
+                //- Update data for this point
+                solidData_[ptNum][3] = gradT[celli];
+                solidData_[ptNum][4] = dTdt_.oldTime()[celli];
+                solidData_[ptNum][6]
+                    = time - deltaT * min(max((Tl_ - T) / (T0 - T), 0), 1);
+                solidData_[ptNum][7] += deltaT * min(max((Tl_ - T) / (T0 - T), 0), 1);
             }
             else
             {
                 R_[celli] = dTdt_.oldTime()[celli];
                 G_[celli] = gradT[celli];
                 
-                if (trackMelting_)
-                {
-                    ts_[celli] = time - deltaT * min(max((Tl_ - T) / (T0 - T), 0), 1);
-                }
+                ts_[celli]
+                    = time - deltaT * min(max((Tl_ - T) / (T0 - T), 0), 1);
+                    
+                tam_[celli] += deltaT - deltaT * min(max((Tl_ - T) / (T0 - T), 0), 1);
             }
 
             ++nSolidificationEvents;
         }
         
-        //- Check for melting events if trackMelting is set
-        if (trackMelting_ && (T0 < Tl_) && (T >= Tl_))
+        //- Check for melting events
+        if ((T0 < Tl_) && (T >= Tl_))
         {
             if (AMR_)
             {
-                vector C = mesh_.C()[celli];
-                
-                List<scalar> eventi(4);
-                
-                eventi[0] = C[0];
-                eventi[1] = C[1];
-                eventi[2] = C[2];
-                eventi[3] = time;
-                
-                meltingEvents_.append(eventi);
+                solidData_[ptNum][5]
+                    = time - deltaT * min(max((T - Tl_) / (T - T0), 0), 1);
+                    
+                solidData_[ptNum][7]
+                    += deltaT * min(max((T - Tl_) / (T - T0), 0), 1);
             }
             else
             {
-                tm_[celli] = time - deltaT * min(max((T - Tl_) / (T - T0), 0), 1);
+                tm_[celli]
+                    = time - deltaT * min(max((T - Tl_) / (T - T0), 0), 1);
+                    
+                tam_[celli]
+                    += deltaT * min(max((T - Tl_) / (T - T0), 0), 1);
             }
             
             ++nMeltingEvents;
@@ -270,15 +367,6 @@ bool Foam::functionObjects::solidificationData::execute()
 
     Info << "solidificationData recorded " << nSolidificationEvents 
          << " solidification events." << endl;
-         
-    if (trackMelting_)
-    {
-        Info << "solidificationData recorded " << nMeltingEvents
-             << " melting events." << endl;
-    }
-
-    //- Clean cells which have been remelted (not yet implemented)
-    //removeRemelts();
 
     return true;
 }
@@ -309,45 +397,67 @@ bool Foam::functionObjects::solidificationData::write()
                     + Foam::name(Pstream::myProcNo()) + ".csv");
 
         //- Write header
-        os << "x,y,z,t,R,G\n";
+        os << "x,y,z,G,R,tm,ts,tam\n";
 
         //- Write each event in series to file
-        for (int i = 0; i < solidificationEvents_.size(); ++i)
+        for (int i = 0; i < solidData_.size(); ++i)
         {
-            int n = solidificationEvents_[i].size() - 1;
+            //Info << "writing line " << i << " of " << solidData_.size() << endl;
+            int n = solidData_[i].size() - 1;
 
             for (int j = 0; j < n; ++j)
             {
-                os << solidificationEvents_[i][j] << ",";
+                os << solidData_[i][j] << ",";
             }
 
-            os << solidificationEvents_[i][n] << "\n";
+            os << solidData_[i][n] << "\n";
         }
+    }
+    
+    else if (writeCsv_)
+    {
+        //- Get current time
+        const fileName currTime = Foam::name(mesh_.time().value());
 
-        solidificationEvents_.clear();
+        //- Create file path for current time data
+        const fileName currTimePath(mesh_.time().rootPath()
+                                    /mesh_.time().globalCaseName()
+                                    /"solidificationData"/currTime);
+                                    
+        mkDir(currTimePath);
+
+        //- Open file for each proc
+        OFstream os(currTimePath + "/" + "data_" 
+                    + Foam::name(Pstream::myProcNo()) + ".csv");
+
+        //- Write header
+        os << "x,y,z,G,R,tm,ts,tam\n";
         
-        if (trackMelting_)
+        const auto& meshC = mesh_.C();
+        const pointField& points = mesh_.points();
+        
+        for (int i = 0; i < mesh_.nCells(); ++i)
         {
-            const fileName meltingDir(currTimePath/"meltingEvents");
+            //- Check that current cell is within bounding box
+            treeBoundBox cellBb(point::max, point::min);
             
-            mkDir(meltingDir);
+            const labelList& vertices = mesh_.cellPoints()[i];
             
-            OFstream mos(meltingDir + "/" + "data_"
-                         + Foam::name(Pstream::myProcNo()) + ".csv");
-                         
-            mos << "x,y,z,t\n";
-            
-            for (int i = 0; i < meltingEvents_.size(); ++i)
+            forAll(vertices, j)
             {
-                int n = meltingEvents_[i].size() - 1;
-
-                for (int j = 0; j < n; ++j)
-                {
-                    os << meltingEvents_[i][j] << ",";
-                }
-
-                os << meltingEvents_[i][n] << "\n";
+                cellBb.min() = min(cellBb.min(), points[vertices[j]]);
+                cellBb.max() = max(cellBb.max(), points[vertices[j]]);
             }
+            
+            if (!cellBb.overlaps(bb_))
+            {
+                continue;
+            }
+        
+            const auto& C = meshC[i];
+            
+            os << C[0] << "," << C[1] << "," << C[2] << "," << G_[i] << ","
+               << R_[i] << "," << tm_[i] << "," << ts_[i] << "," << tam_[i] << endl;
         }
     }
 
